a2ada7501ef39d4b8bcd5be19750530dea2e4161
ggakila <gisorehaggai@gmail.com>
2023-08-05 05:41:10 +0300

    concat comments added floor

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -5,6 +5,8 @@ concat fxn takes str1 and str2 and returns a concatenated string
 
 
 def concat(str1: str, str2: str) -> str:
-
+    '''
+    function comments
+    '''
 
     return str1 + str2
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+function takes in float and return floor of the float which in this case will be an int
+'''
+
+
+def floor(n: float) -> int:
+    '''
+    floor makes it an int
+    '''
+    return int(n)
