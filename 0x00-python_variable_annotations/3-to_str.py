5c79b11abfe8b3067d91b55f9760a0044f1d4416
ggakila <gisorehaggai@gmail.com>
2023-08-05 05:50:44 +0300

    returns string from the passed floats

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+takes a float n as argument
+returns the string representation of the float.
+'''
+
+
+def to_str(n: float) -> str:
+    '''
+    Float to string
+    '''
+    return str(n)
